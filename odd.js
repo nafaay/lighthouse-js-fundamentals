0483c30a35c6dbad890a5e3c04bc5e4f58a557f9
Youssef NAFAA <nafaayoussef@gmail.com>
2021-10-04 00:13:58 +0000

    odd function

---
+++ odd.js
@@ -0,0 +1,12 @@
+const isOdd = function (num) {
+	return !(num % 2 === 0);
+};
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("4 is odd: " + isOdd(4));
+console.log("6 is odd: " + isOdd(6));
+console.log("11 is odd: " + isOdd(11));
+console.log("7 is odd: " + isOdd(7));
+console.log("2 is odd: " + isOdd(2));
+console.log("15 is odd: " + isOdd(15));
